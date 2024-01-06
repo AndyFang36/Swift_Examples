0c5971e9113bbde50f3cc6b0f92a5cfc778103f7
AndyFang36 <andylovesapple666@icloud.com>
2024-01-07 00:04:37 +0800

    while and repeat

--- C5_Control_Flow/C5_Control_Flow/main.swift
+++ C5_Control_Flow/C5_Control_Flow/main.swift
@@ -43,9 +43,9 @@ print("")
 // half-open range operator (..<)
 let minutes = 60
 for tickMark in 0..<minutes {
-    // render the tick mark each minute (60 times)
+    print(tickMark, terminator: ", ")
 }
-print("")
+print("\n")
 // Use the stride(from:to:by:) function to skip the unwanted marks.
 let minuteInterval = 15
 for tickMark in stride(from: 0, to: minutes, by: minuteInterval) {
@@ -82,4 +82,96 @@ while square < finalSquare {
     }
     print(square)
 }
-print("Game over!")
+print("Game over!\n")
+
+/** Repeat-While (do-while) */
+/* Here’s the Snakes and Ladders example again,
+   written as a repeat-while loop rather than a while loop.
+ */
+square = 0  // reset to 0
+diceRoll = 0  // reset to 0
+repeat {
+    // move up or down for a snake or ladder
+    square += board[square]
+    print("at square \(square)", terminator: ", ")
+    // roll the dice
+    diceRoll += 1
+    if diceRoll == 7 { diceRoll = 1 }
+    // move by the rolled amount
+    square += diceRoll
+    print("after dice: ", square)
+} while square < finalSquare
+print("Game over!\n")
+
+
+// Conditional Statements
+// if
+var temperatureInFahrenheit = 30
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+}
+// if-else
+temperatureInFahrenheit = 40
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else {
+    print("It's not that cold. Wear a T-shirt.")
+}
+// if -> else-if -> else
+temperatureInFahrenheit = 90
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+} else {
+    print("It's not that cold. Wear a T-shirt.")
+}
+// else is optional
+temperatureInFahrenheit = 72
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+}
+// setting values
+let temperatureInCelsius = 25
+let weatherAdvice: String
+if temperatureInCelsius <= 0 {
+    weatherAdvice = "It's very cold. Consider wearing a scarf."
+} else if temperatureInCelsius >= 30 {
+    weatherAdvice = "It's really warm. Don't forget to wear sunscreen."
+} else {
+    weatherAdvice = "It's not that cold. Wear a T-shirt."
+}
+print(weatherAdvice)
+// if expression
+let weatherAdvice2 = if temperatureInCelsius <= 0 {
+    "It's very cold. Consider wearing a scarf."
+} else if temperatureInCelsius >= 30 {
+    "It's really warm. Don't forget to wear sunscreen."
+} else {
+    "It's not that cold. Wear a T-shirt."
+}
+print(weatherAdvice2)
+// you need to specify the type explicitly
+let freezeWarning: String? = if temperatureInCelsius <= 0 {
+    "It's below freezing. Watch for ice!"
+} else {
+    nil
+}
+print(freezeWarning!)
+//
+let freezeWarning2 = if temperatureInCelsius <= 0 {
+    "It's below freezing. Watch for ice!"
+} else {
+    nil as String?
+}
+print(freezeWarning2!)
+//
+let weatherAdvice3 = if temperatureInCelsius > 100 {
+    throw fatalError()
+    // throw TemperatureError.boiling
+} else {
+    "It's a reasonable temperature."
+}
+print(weatherAdvice3)
