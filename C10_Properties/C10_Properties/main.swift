0847ba091947a7b11f5b48f4c00fdf3cc4f030e8
AndyFang36 <andylovesapple666@icloud.com>
2024-01-16 00:15:58 +0800

    Chapter 10: properties

---
+++ C10_Properties/C10_Properties/main.swift
@@ -0,0 +1,310 @@
+//
+//  main.swift
+//  C10_Properties
+//
+//  Created by Andy Fang on 14/01/2024.
+//
+
+import Foundation
+
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue = 6
+/** Stored Properties of Constant Structure Instances */
+// If you create an instance of a structure and assign that instance to a constant, you can’t modify the instance’s properties, even if they were declared as variable properties
+let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
+// this range represents integer values 0, 1, 2, and 3
+//! rangeOfFourItems.firstValue = 6
+
+// Lazy Stored Properties
+class DataImporter {
+    /*
+     DataImporter is a class to import data from an external file.
+     The class is assumed to take a nontrivial amount of time to initialize.
+     */
+    var filename = "data.txt"
+    // the DataImporter class would provide data importing functionality here
+}
+class DataManager {
+    lazy var importer = DataImporter()
+    var data: [String] = []
+    // the DataManager class would provide data management functionality here
+}
+let manager = DataManager()
+manager.data.append("Some data")
+manager.data.append("Some more data")
+print(manager.self)
+print(manager.importer.filename)
+
+// Computed Properties
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+var square = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 10.0, height: 10.0))
+let initialSquareCenter = square.center
+square.center = Point(x: 15.0, y: 15.0)
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
+// Shorthand Setter Declaration
+struct AlternativeRect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
+// Shorthand Getter Declaration
+struct CompactRect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            Point(x: origin.x + (size.width / 2), y: origin.y + (size.height / 2))
+        }
+        set {
+            origin.x = newValue.x - (size.width / 2)
+            origin.y = newValue.y - (size.height / 2)
+        }
+    }
+}
+
+// Read-Only Computed Properties
+struct Cuboid {
+    var width = 0.0, height = 0.0, depth = 0.0
+    var volume: Double {
+        return width * height * depth
+    }
+}
+let fourByFiveByTwo = Cuboid(width: 4.0, height: 5.0, depth: 2.0)
+print("the volume of fourByFiveByTwo is \(fourByFiveByTwo.volume)")
+
+
+/** Property Observers */
+// willSet is called just before the value is stored.
+// didSet is called immediately after the new value is stored.
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue  {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+let stepCounter = StepCounter()
+stepCounter.totalSteps = 200
+stepCounter.totalSteps = 360
+stepCounter.totalSteps = 896
+
+
+/** Property Wrappers */
+@propertyWrapper
+struct TwelveOrLess {
+    private var number = 0
+    var wrappedValue: Int {
+        get { number }
+        set { number = min(newValue, 12) }
+    }
+}
+
+struct SmallRectangle {
+    @TwelveOrLess var height: Int
+    @TwelveOrLess var width: Int
+}
+
+var rectangle = SmallRectangle()
+print(rectangle.height)
+rectangle.height = 10
+print(rectangle.height)
+rectangle.height = 24
+print(rectangle.height)
+// without @twelveOrLess
+struct SmallRectangle2 {
+    private var _height = TwelveOrLess()
+    private var _width = TwelveOrLess()
+    var height: Int {
+        get { _height.wrappedValue }
+        set { _height.wrappedValue = newValue }
+    }
+    var width: Int {
+        get { _width.wrappedValue }
+        set { _width.wrappedValue = newValue }
+    }
+}
+
+
+/** Setting Initial Values for Wrapped Properties */
+@propertyWrapper
+struct SmallNumber {
+    private var maximum: Int
+    private var number: Int
+    
+    var wrappedValue: Int {
+        get { number }
+        set { number = min(newValue, maximum) }
+    }
+    
+    init() {
+        maximum = 12
+        number = 0
+    }
+    init(wrappedValue: Int) {
+        maximum = 12
+        number = min(wrappedValue, maximum)
+    }
+    init(wrappedValue: Int, maximum: Int) {
+        self.maximum = maximum
+        number = min(wrappedValue, maximum)
+    }
+}
+
+// eg.
+struct ZeroRectangle {
+    @SmallNumber var height: Int
+    @SmallNumber var width: Int
+}
+
+var zeroRectangle = ZeroRectangle()
+print(zeroRectangle.height, zeroRectangle.width)
+
+// eg.
+struct UnitRectangle {
+    @SmallNumber var height: Int = 1
+    @SmallNumber var width: Int = 1
+}
+
+var unitRectangle = UnitRectangle()
+print(unitRectangle.height, unitRectangle.width)
+
+// eg.
+struct NarrowRectangle {
+    @SmallNumber(wrappedValue: 2, maximum: 5) var height: Int
+    @SmallNumber(wrappedValue: 3, maximum: 4) var width: Int
+}
+
+var narrowRectangle = NarrowRectangle()
+print(narrowRectangle.height, narrowRectangle.width)
+narrowRectangle.height = 100
+narrowRectangle.width = 100
+print(narrowRectangle.height, narrowRectangle.width)
+
+// eg.
+struct MixedRectangle {
+    @SmallNumber var height: Int = 1
+    @SmallNumber(maximum: 9) var width: Int = 2
+}
+
+var mixedRectangle = MixedRectangle()
+print(mixedRectangle.height)
+mixedRectangle.height = 20
+print(mixedRectangle.height)
+
+
+/** Projecting a Value From a Property Wrapper */
+@propertyWrapper
+struct SmallNumberV2 {
+    private var number: Int
+    private(set) var projectedValue: Bool
+    
+    var wrappedValue: Int {
+        get { number }
+        set {
+            if newValue > 12 {
+                number = 12
+                projectedValue = true
+            } else {
+                number = newValue
+                projectedValue = false
+            }
+        }
+    }
+
+    init() {
+        self.number = 0
+        self.projectedValue = false
+    }
+}
+
+struct SomeStructure {
+    @SmallNumberV2 var someNumber: Int
+}
+
+var someStructure = SomeStructure()
+someStructure.someNumber = 4
+print(someStructure.$someNumber)
+someStructure.someNumber = 55
+print(someStructure.$someNumber)
+
+//
+enum Size2 {
+    case small, large
+}
+
+struct SizedRectangle {
+    @SmallNumberV2 var height: Int
+    @SmallNumberV2 var width: Int
+
+    mutating func resize(to size: Size2) -> Bool {
+        switch size {
+            case .small:
+                height = 10
+                width = 20
+            case .large:
+                height = 100
+                width = 100
+        }
+        return $height || $width
+    }
+}
+
+var sizedRec = SizedRectangle()
+if sizedRec.resize(to: .small) {
+    print(sizedRec.height, sizedRec.width)
+}
+print("")
+
+
+/** Global and Local Variables */
+func someFunction() {
+    @SmallNumber var myNumber: Int = 0
+    print(myNumber)
+    
+    myNumber = 10
+    print(myNumber)
+    
+    myNumber = 24
+    print(myNumber)
+}
+someFunction()
+print("")
